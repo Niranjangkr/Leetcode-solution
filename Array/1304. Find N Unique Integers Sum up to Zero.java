a3e4aa97bc6d45c3fcff9e6d0e4a437d8c0faa0d
Niranjan Gaonkar <niranjangaonkar.09@gmail.com>
2023-07-21 08:43:52 +0530

    one new approach

--- Array/1304. Find N Unique Integers Sum up to Zero.java
+++ Array/1304. Find N Unique Integers Sum up to Zero.java
@@ -57,3 +57,60 @@ class Solution {
     - till mid+1 index to n.length -1 give positive number
     - starting from 1 , 2, 3 like this only thing the last index should have value of n; 
  */
+
+// approach 2
+
+class Solution {
+    public int[] sumZero(int n) {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = i * 2 - n + 1;
+        }
+        return arr;
+    }
+}
+
+/**
+
+    second approach is look at the pattern it makes that is the output arry makes an wave like output with constant difference so we just have to increment the array with that difference 
+    ie like for:
+    n = 5
+    -4 -2 0 2 4 
+    this is the output array we can have 
+    n = 3, [-2, 0, 2]
+    n = 4, [-3, -1, 1, 3]
+    n = 5, [-4, -2, 0, 2, 4]
+
+    so the it spreads like a wave so the delta or difference here is 2
+
+    so if we use the below rule we get is 
+    A[i] = i * 2 - n + 1;
+ */
+
+// approach 3
+
+class Solution {
+    public int[] sumZero(int n) {
+        int[] arr = new int[n];
+        if(n % 2 == 0){
+             for(int i = 0; i < n; i += 2){
+                 arr[i] = i+1;
+                 arr[i+1] = -(i+1);
+             }
+        }else{
+            arr[0] = 0;
+            for(int i = 1; i < n; i += 2){
+                 arr[i] = i+1;
+                 arr[i+1] = -(i+1);
+             }
+        }
+        return arr;
+    }
+}
+
+/**
+    also we can do another approach that is in the array we can make pair of each number like below 
+    1, -1, 2, -2, 3, -3
+    with this we get zero but we cant make this pair when n is odd so for odd what we do is make the first element zero and the rest than can be pairs
+ */
+
